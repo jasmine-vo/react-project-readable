a122322f890c23a91eb7dc116988a71059d43211
“Jasmine <jasmine.j.jackson03@gmail.com>
2018-02-13 05:44:41 +0000

    feat: add buttons for each category

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,25 +3,25 @@ import * as API from '../utils/api';
 
 class App extends Component {
   state = {
-      categories: []
-    }
+    categories: []
+  }
 
+// load categories from API after component is mounted
   componentDidMount() {
     API.getCategories().then((data) => {
       this.setState({ categories:data})
     })
   }
-  
+
   render() {
     return (
       <div>
-        <ul>
-          {this.state.categories.map((category) => (
-            <li key={category}>
-              {category}
-            </li>
-          ))}
-        </ul>
+        <h1>Readable</h1>
+        {this.state.categories.map((category) => (
+        <button key={category}>
+          {category}
+        </button>
+        ))}
       </div>
     );
   }
