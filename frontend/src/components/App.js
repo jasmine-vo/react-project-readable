0eb8d558eca48acca0ac9afafea56228287e4635
“Jasmine-Vo” <jasmine.j.jackson03@gmail.com>
2018-02-14 02:59:42 +0000

    feat: add control that allows user to sort by date or vote score

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import ListPosts from './ListPosts.js';
 import * as API from '../utils/api';
+import sortBy from 'sort-by';
 
 class App extends Component {
   state = {
@@ -17,8 +18,20 @@ class App extends Component {
     })
     API.getPosts().then((data) => {
       console.log(data)
-      this.setState({ posts:data })
+      this.setState({ posts:data.sort(sortBy('-timestamp')) })
     })
+  }
+
+  sortPosts = (values) => {
+    
+    const value = values.split(',')[0];
+    const reversed = values.split(',')[1] === 'reverse';
+
+    if (reversed) {
+      this.setState((state) => ({ posts: this.state.posts.sort(sortBy(`-${value}`)) }))
+    } else {
+      this.setState((state) => ({ posts: this.state.posts.sort(sortBy(value)) }))
+    }
   }
 
   render() {
@@ -37,6 +50,15 @@ class App extends Component {
             >{category}</Link>
           </button>
         ))}
+		  <select
+            defaultValue="timestamp,reverse"
+			onChange={(event) => this.sortPosts(event.target.value)}>
+            <option value="none" disabled>Sort by...</option>
+            <option value="timestamp">Oldest to newest</option>
+			<option value="timestamp,reverse">Newest to oldest</option>
+            <option value="voteScore">Lowest to highest score</option>
+			<option value="voteScore,reverse">Highest to lowest score</option>
+          </select>
         <Route exact path="/" render={() => (
           <div>
             <ListPosts
