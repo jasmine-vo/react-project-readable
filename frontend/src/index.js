732a0c6a6c520a1da28af9f583fe9be379a6637a
“Jasmine-Vo” <jasmine.j.jackson03@gmail.com>
2018-03-06 01:50:20 +0000

    feat: add Redux DevTools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,10 @@ import App from './components/App';
 import { createStore } from 'redux';
 import reducer from './reducers'
 
-const store = createStore(reducer)
-
-console.log(store.getState())
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION()
+)
 
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
