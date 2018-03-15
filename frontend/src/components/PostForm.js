a11af3f8317ecb200c291026761afc8cfc5530bf
“Jasmine-Vo” <jasmine.j.jackson03@gmail.com>
2018-03-15 21:22:05 +0000

    feat: add event handlers to post form

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 
 class PostForm extends Component {
 
+	state = {
+    title: '',
+    author: '',
+    category: '',
+    body: ''
+  }
+
+  handleChange = (event) => {
+  	this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+  	alert(this.state.title);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Title:
-            <input type="text" name="title" />
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
           </label><br />
           <label>
             Author:
-            <input type="text" name="author" />
+            <input type="text" name="author" value={this.state.author} onChange={this.handleChange} />
           </label><br />
           <label>
             Category:
-            <select defaultValue="none">
+            <select defaultValue="none" name="category" value={this.state.category} onChange={this.handleChange}>
                 <option value="none" disabled></option>
                 {this.props.categories.map((category, index) => (
                   <option value={category} key={index}>{category}</option>
@@ -26,9 +42,9 @@ class PostForm extends Component {
           </label>
           <label>
             Body:
-            <input type='text' name='body' />
+            <input type="text" name="body" value={this.state.body} onChange={this.handleChange} />
           </label><br />
-          <input type='submit' value='Submit' />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     )
