979304dc485f757d98e5585124ccc3a8cda770cc
“Jasmine-Vo” <jasmine.j.jackson03@gmail.com>
2018-03-16 01:26:58 +0000

    feat: use Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
-import reducer from './reducers'
+import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter>
+  	<Provider store={store}>
+  		<App />
+  	</Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
