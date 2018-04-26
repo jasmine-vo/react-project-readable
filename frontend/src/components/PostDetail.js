08017805cf9b1a46a3f067baf67e630f711b10d1
“Jasmine-Vo” <jasmine.j.jackson03@gmail.com>
2018-04-26 23:01:31 +0000

    feat: add icons to PostDetail

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import * as API from '../utils/api';
 import { toDate } from '../utils/helpers';
 import { connect } from 'react-redux';
-import { updatePostScore, getPostDetails, getComments, deleteComment, updateCommentScore } from '../actions';
+import { 
+  updatePostScore,
+  getPostDetails,
+  getComments,
+  deleteComment,
+  updateCommentScore
+} from '../actions';
 import Modal from 'react-modal';
 import PostForm from './PostForm';
 import CommentForm from './CommentForm';
 import { withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import PencilIcon from '../icons/pencil.svg';
+import DeleteIcon from '../icons/garbage.svg';
+import RedHeartIcon from '../icons/favorite.svg';
+import GreyHeartIcon from '../icons/favorite-grey.svg';
 
 class PostDetail extends Component {
   state = {
@@ -73,68 +84,150 @@ class PostDetail extends Component {
 
   render() {
 
+    const { post, comments } = this.props
+    const { postModalOpen, commentId, editCommentFormOpen } = this.state
+
     return (
-      <div>
-        <button onClick={() => this.props.history.goBack()}>
-          Back
-        </button>
-        <h3>{this.props.post.title}</h3>
-        submitted on {toDate(this.props.post.timestamp)} by {this.props.post.author}
-        <p>{this.props.post.body}</p>
-        <button onClick={() => this.openPostModal()}>
-          Edit Post
-        </button>
-        <button onClick={() => this.handleDeletePost()}>
-          Delete Post
-        </button>
-        <p>vote score: {this.props.post.voteScore}</p>
-        <button onClick={() => this.handlePostVote('downVote')}>
+      <div className='post-details-container'>
+        <div className='nav'>
+          <button
+            className='nav-button'>
+            <Link
+              to='/'
+              className='path-link'
+            >home</Link>
+          </button>
           -
-        </button>
-        <button onClick={() => this.handlePostVote('upVote')}>
-          +
-        </button>
-        <h4>Add Comment</h4>
-        <CommentForm 
-          parentId={this.props.id}
-          displayForm={true}
-          editMode={false}
-        />
-        {this.props.comments ?
-          <div>There are {this.props.comments.length} comments about this post
-            <ul>
-              {this.props.comments.map((comment) => (
-                <li key={comment.id}>
-                  {comment.body}<br />
-                  posted by {comment.author} on {toDate(comment.timestamp)}<br />
-                  <p>vote score: {comment.voteScore}</p>
-                  <button onClick={() => this.handleCommentVote('downVote', comment.id)}>
-                    -
-                  </button>
-                  <button onClick={() => this.handleCommentVote('upVote', comment.id)}>
-                    +
-                  </button>
-                  <button onClick={() => this.openEditCommentForm(comment.id)}>
-                    Edit
-                  </button>
-                  <button onClick={() => this.handleDeleteComment(comment.id)}>
-                    Delete
-                  </button>
-                  <CommentForm
-                    displayForm={this.state.editCommentFormOpen && (comment.id === this.state.commentId)}
-                    editMode={true}
-                    commentId={comment.id}
-                    onCloseEditCommentForm={this.closeEditCommentForm}
-                  />
-                </li>
-              ))}
-            </ul>
+          <button
+            className='nav-button'>
+            <Link
+              to={`/${post.category}`}
+              className='path-link'
+            >{post.category}</Link>
+          </button>
+          -
+          <button
+            className='nav-button'>
+            <Link
+              to={`/${post.category}/${post.id}`}
+              className='path-link'
+            >{post.title}</Link>
+          </button>
+        </div>
+
+        <div className='post-container'>
+          <div className='post-header'>
+            <h3 className='post-subtitle'>{post.title}</h3>
+            <span>
+              submitted by {post.author} on {toDate(post.timestamp)} 
+            </span>
           </div>
-        : `There are 0 comments about this post`}
+
+          <div className='post-body'>
+            {post.body}
+          </div>
+
+          <div className='edit-delete'>
+            <button
+              onClick={() => this.openPostModal()}
+              className='small-button'>
+              <img className='icon' src={PencilIcon} alt='pencil-icon' />
+            </button>
+            <button
+              onClick={() => this.handleDeletePost()}
+              className='small-button'>
+              <img className='icon' src={DeleteIcon} alt='delete-icon' />
+            </button>
+          </div>
+          <div className='vote-score'>
+            {(post.voteScore > 0) ?
+              <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
+            : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
+            <span> {post.voteScore} </span>
+            <button
+              onClick={() => this.handlePostVote('upVote')}
+              className='small-button'>
+              +
+            </button>
+            <button
+              onClick={() => this.handlePostVote('downVote')}
+              className='small-button'>
+              -
+            </button>
+          </div>
+        </div>
+
+        <div className='post-container'>
+          <div className='comment-form'>
+
+            <h3 className='subtitle'>Add a comment</h3>
+
+            <CommentForm 
+              parentId={this.props.id}
+              displayForm={true}
+              editMode={false}
+            />
+
+            <h3 className='subtitle'>Comments ({comments.length})</h3>
+          </div>
+
+          {comments ?
+            <div className='comment-list'>
+
+              <ul className='comments'>
+                {comments.map((comment) => (
+                  <li className='comments' key={comment.id}>
+                    <div className='comment-details'>
+                      <p>{comment.body}</p>
+
+                      <span>
+                        submitted by {comment.author} on {toDate(comment.timestamp)}
+                      </span>
+
+                    </div>
+
+                    <div className='edit-delete'>
+                      <button
+                        onClick={() => this.openEditCommentForm(comment.id)}
+                        className='small-button'>
+                        <img className='icon' src={PencilIcon} alt='pencil-icon' />
+                      </button>
+                      <button
+                        onClick={() => this.handleDeleteComment(comment.id)}
+                        className='small-button'>
+                        <img className='icon' src={DeleteIcon} alt='delete-icon' />
+                      </button>
+                    </div>
+                    <div className='vote-score'>
+                      {(comment.voteScore > 0) ?
+                        <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
+                      : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
+                      <span> {comment.voteScore} </span>
+                      <button onClick={() => this.handleCommentVote('upVote', comment.id)}>
+                        +
+                      </button>
+                      <button onClick={() => this.handleCommentVote('downVote', comment.id)}>
+                        -
+                      </button>
+                    </div>
+                    <div className='comment-form'>
+                      <CommentForm
+                        displayForm={editCommentFormOpen && (comment.id === commentId)}
+                        editMode={true}
+                        commentId={comment.id}
+                        onCloseEditCommentForm={this.closeEditCommentForm}
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          : <span>There are 0 comments about this post</span>}
+          </div>
         <Modal
           className='modal'
           overlayClassName='overlay'
-          isOpen={this.state.postModalOpen}
+          isOpen={postModalOpen}
           onRequestClose={this.closePostModal}
           contentLabel='Modal'
         >
@@ -142,10 +235,13 @@ class PostDetail extends Component {
             <h3>Edit Post</h3>
             <PostForm
               onClosePostModal={this.closePostModal}
-              post={this.props.post}
+              post={post}
+              category={post.category}
             />
-            <button onClick={() => this.closePostModal()}>
-              Cancel
+            <button
+              onClick={() => this.closePostModal()}
+              className='cancel-button'>
+                Cancel
             </button>
           </div>
         </Modal>
