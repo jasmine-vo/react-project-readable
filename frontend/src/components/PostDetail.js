7204b0e77f916c02393304102d4513c506d098f7
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-05-10 22:59:33 -0700

    fix: add 404 page if post is not found

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -92,161 +92,175 @@ class PostDetail extends Component {
 
     return (
       <div className='post-details-container'>
-        <div className='nav'>
-          <button
-            className='nav-button'>
-            <Link
-              to='/'
-              className='path-link'
-            >home</Link>
-          </button>
-          -
-          <button
-            className='nav-button'>
-            <Link
-              to={`/${post.category}`}
-              className='path-link'
-            >{post.category}</Link>
-          </button>
-          -
-          <button
-            className='nav-button'>
-            <Link
-              to={`/${post.category}/${post.id}`}
-              className='path-link'
-            >{post.title}</Link>
-          </button>
-        </div>
-
-        <div className='post-container'>
-          <div className='post-header'>
-            <h3 className='post-subtitle'>{post.title}</h3>
-            <span>
-              submitted by {post.author} on {toDate(post.timestamp)} 
-            </span>
-          </div>
-
-          <div className='post-body'>
-            {post.body}
-          </div>
-
-          <div className='edit-delete'>
-            <button
-              onClick={() => this.openPostModal()}
-              className='small-button'>
-              <img className='icon' src={PencilIcon} alt='pencil-icon' />
-            </button>
-            <button
-              onClick={() => this.handleDeletePost()}
-              className='small-button'>
-              <img className='icon' src={DeleteIcon} alt='delete-icon' />
-            </button>
-          </div>
-          <div className='vote-score'>
-            {(post.voteScore > 0) ?
-              <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
-            : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
-            <span> {post.voteScore} </span>
-            <button
-              onClick={() => this.handlePostVote('upVote')}
-              className='small-button'>
-              +
-            </button>
-            <button
-              onClick={() => this.handlePostVote('downVote')}
-              className='small-button'>
-              -
-            </button>
-          </div>
-        </div>
-
-        <div className='post-container'>
-          <div className='comment-form'>
-
-            <h3 className='subtitle'>Add a comment</h3>
-
-            <CommentForm 
-              parentId={this.props.id}
-              displayForm={true}
-              editMode={false}
-            />
-
-            <h3 className='subtitle'>Comments ({comments.length})</h3>
-          </div>
-
-          {comments ?
-            <div className='comment-list'>
-
-              <ul className='comments'>
-                {comments.map((comment) => (
-                  <li className='comments' key={comment.id}>
-                    <div className='comment-details'>
-                      <p>{comment.body}</p>
-
-                      <span>
-                        submitted by {comment.author} on {toDate(comment.timestamp)}
-                      </span>
-
-                    </div>
-
-                    <div className='edit-delete'>
-                      <button
-                        onClick={() => this.openEditCommentForm(comment.id)}
-                        className='small-button'>
-                        <img className='icon' src={PencilIcon} alt='pencil-icon' />
-                      </button>
-                      <button
-                        onClick={() => this.handleDeleteComment(comment.id)}
-                        className='small-button'>
-                        <img className='icon' src={DeleteIcon} alt='delete-icon' />
-                      </button>
-                    </div>
-                    <div className='vote-score'>
-                      {(comment.voteScore > 0) ?
-                        <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
-                      : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
-                      <span> {comment.voteScore} </span>
-                      <button onClick={() => this.handleCommentVote('upVote', comment.id)}>
-                        +
-                      </button>
-                      <button onClick={() => this.handleCommentVote('downVote', comment.id)}>
-                        -
-                      </button>
-                    </div>
-                    <div className='comment-form'>
-                      <CommentForm
-                        displayForm={editCommentFormOpen && (comment.id === commentId)}
-                        editMode={true}
-                        commentId={comment.id}
-                        onCloseEditCommentForm={this.closeEditCommentForm}
-                      />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          : <span>There are 0 comments about this post</span>}
-          </div>
-        <Modal
-          className='modal'
-          overlayClassName='overlay'
-          isOpen={postModalOpen}
-          onRequestClose={this.closePostModal}
-          contentLabel='Modal'
-        >
+        {(!post.id) ?
           <div>
-            <PostForm
-              onClosePostModal={this.closePostModal}
-              post={post}
-              category={post.category}
-            />
-            <button
-              onClick={() => this.closePostModal()}
-              className='cancel-button'>
-                Cancel
+            <h1>404</h1>
+            Sorry, the page you requested cannot be found.
+            <button className='nav-button'>
+              <Link
+                to='/'
+                className='path-link'
+              >Go to homepage</Link>
             </button>
           </div>
-        </Modal>
+        : <div>
+            <div className='nav'>
+              <button
+                className='nav-button'>
+                <Link
+                  to='/'
+                  className='path-link'
+                >home</Link>
+              </button>
+              -
+              <button
+                className='nav-button'>
+                <Link
+                  to={`/${post.category}`}
+                  className='path-link'
+                >{post.category}</Link>
+              </button>
+              -
+              <button
+                className='nav-button'>
+                <Link
+                  to={`/${post.category}/${post.id}`}
+                  className='path-link'
+                >{post.title}</Link>
+              </button>
+            </div>
+
+            <div className='post-container'>
+              <div className='post-header'>
+                <h3 className='post-subtitle'>{post.title}</h3>
+                <span>
+                  submitted by {post.author} on {toDate(post.timestamp)} 
+                </span>
+              </div>
+
+              <div className='post-body'>
+                {post.body}
+              </div>
+
+              <div className='edit-delete'>
+                <button
+                  onClick={() => this.openPostModal()}
+                  className='small-button'>
+                  <img className='icon' src={PencilIcon} alt='pencil-icon' />
+                </button>
+                <button
+                  onClick={() => this.handleDeletePost()}
+                  className='small-button'>
+                  <img className='icon' src={DeleteIcon} alt='delete-icon' />
+                </button>
+              </div>
+              <div className='vote-score'>
+                {(post.voteScore > 0) ?
+                  <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
+                : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
+                <span> {post.voteScore} </span>
+                <button
+                  onClick={() => this.handlePostVote('upVote')}
+                  className='small-button'>
+                  +
+                </button>
+                <button
+                  onClick={() => this.handlePostVote('downVote')}
+                  className='small-button'>
+                  -
+                </button>
+              </div>
+            </div>
+
+            <div className='post-container'>
+              <div className='comment-form'>
+
+                <h3 className='subtitle'>Add a comment</h3>
+
+                <CommentForm 
+                  parentId={this.props.id}
+                  displayForm={true}
+                  editMode={false}
+                />
+
+                <h3 className='subtitle'>Comments ({comments.length})</h3>
+              </div>
+
+              {comments ?
+                <div className='comment-list'>
+
+                  <ul className='comments'>
+                    {comments.map((comment) => (
+                      <li className='comments' key={comment.id}>
+                        <div className='comment-details'>
+                          <p>{comment.body}</p>
+
+                          <span>
+                            submitted by {comment.author} on {toDate(comment.timestamp)}
+                          </span>
+
+                        </div>
+
+                        <div className='edit-delete'>
+                          <button
+                            onClick={() => this.openEditCommentForm(comment.id)}
+                            className='small-button'>
+                            <img className='icon' src={PencilIcon} alt='pencil-icon' />
+                          </button>
+                          <button
+                            onClick={() => this.handleDeleteComment(comment.id)}
+                            className='small-button'>
+                            <img className='icon' src={DeleteIcon} alt='delete-icon' />
+                          </button>
+                        </div>
+                        <div className='vote-score'>
+                          {(comment.voteScore > 0) ?
+                            <img className='icon' src={RedHeartIcon} alt='red-heart-icon' />
+                          : <img className='icon' src={GreyHeartIcon} alt='grey-heart-icon' />}
+                          <span> {comment.voteScore} </span>
+                          <button onClick={() => this.handleCommentVote('upVote', comment.id)}>
+                            +
+                          </button>
+                          <button onClick={() => this.handleCommentVote('downVote', comment.id)}>
+                            -
+                          </button>
+                        </div>
+                        <div className='comment-form'>
+                          <CommentForm
+                            displayForm={editCommentFormOpen && (comment.id === commentId)}
+                            editMode={true}
+                            commentId={comment.id}
+                            onCloseEditCommentForm={this.closeEditCommentForm}
+                          />
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              : <span>There are 0 comments about this post</span>}
+              </div>
+            <Modal
+              className='modal'
+              overlayClassName='overlay'
+              isOpen={postModalOpen}
+              onRequestClose={this.closePostModal}
+              contentLabel='Modal'
+            >
+              <div>
+                <PostForm
+                  onClosePostModal={this.closePostModal}
+                  post={post}
+                  category={post.category}
+                />
+                <button
+                  onClick={() => this.closePostModal()}
+                  className='cancel-button'>
+                    Cancel
+                </button>
+              </div>
+            </Modal>
+          </div>
+        }
       </div>
     )
   }
